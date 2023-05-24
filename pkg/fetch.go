ce2d1d5ebbd83ccc5207006f6c10b97da99bd64f
Kaiser925 <zhangliang925@icloud.com>
2023-05-24 21:17:11 +0800

    fix: adapt new image src

--- pkg/fetch.go
+++ pkg/fetch.go
@@ -4,7 +4,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,9 +59,11 @@ func GetCover(u string, filename string) {
 		log.Fatalf("coudn't parse response html: %v\n", err)
 	}
 	meta := htmlquery.FindOne(doc, "//meta[@itemprop='image']")
-	imgSrc := htmlquery.SelectAttr(meta, "content")
+	imgSrc := "https:" + htmlquery.SelectAttr(meta, "content")
 
-	log.Println("downloading image")
+	imgSrc, _, _ = strings.Cut(imgSrc, "@")
+	log.Println("downloading image from", imgSrc)
+
 	resp, err = requests4go.Get(imgSrc, headers)
 	if err != nil {
 		log.Fatalf("coudn't send request to %s: %v\n", u, err)
